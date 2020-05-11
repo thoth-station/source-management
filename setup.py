e7d044ffa5725bfde8774fb2556f41f2cca36fd4
sai <saisankargochhayat@gmail.com>
2020-05-11 17:40:28 -0400

    Fixed license

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setuptools.setup(
     install_requires=get_install_requires(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPLv3+",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
